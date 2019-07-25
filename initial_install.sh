7a5f57e5d7bc48d7eaa83abd861556557d0cfb46
gnus <gnusd@telia.com>
2019-07-25 18:31:48 +0200

    2019-07-25

--- initial_install.sh
+++ initial_install.sh
@@ -3,3 +3,5 @@
 sudo apt install tmux curl pyjoke cowsay
 sudo apt remove --purge vi vim-tiny
 sudo snap install neovim-kalikiana fwupd nextcloud-client yakuake keepassxc
+
+./native_tridactyl.sh
---
+++ native_tridactyl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+curl -fsSl https://raw.githubusercontent.com/tridactyl/tridactyl/master/native/install.sh -o /tmp/trinativeinstall.sh && bash /tmp/trinativeinstall.sh master
