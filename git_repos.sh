33065f20949786fa0d81a9505ee57929b50f8d20
gnus <gnusd@telia.com>
2019-07-25 23:19:10 +0200

    2019-07-25

--- git_repos.sh
+++ git_repos.sh
@@ -4,3 +4,4 @@
 git clone git@github.com:ryanoasis/nerd-fonts.git
 git clone git@github.com:sieren/QSyncthingTray.git
 git clone git@github.com:erpalma/throttled.git
+git clone git@github.com:kwin-scripts/kwin-tiling.git
