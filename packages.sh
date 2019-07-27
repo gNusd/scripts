083c839cbac9d6ad77157c2f6b70a409ecaeda16
gnus <gnusd@telia.com>
2019-07-27 23:10:26 +0200

    2019-07-27

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
 sudo apt autoremove -y
-sudo apt install -y tmux curl pyjoke cowsay python-pip python3-pip neovim neovim-qt
+sudo apt install -y tmux curl pyjoke cowsay python-pip python3-pip neovim neovim-qt kmail
 sudo apt remove --purge -y vi vim-tiny
 sudo snap install cmake --classic
 sudo snap install fwupd --classic
