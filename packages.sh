5f902cbfa6d3ea2808de36c556e9daea38b68caf
gnus <gnusd@telia.com>
2019-08-17 19:04:33 +0200

    2019-08-17

--- packages.sh
+++ packages.sh
@@ -21,7 +21,7 @@ sudo $pacmanager autoremove -y
 gen_pac="tmux curl neovim neovim-qt gimp gimp-help-sv htop zathura redshift yakuake tlp tlp-rdw"
 neon_pac="kontact kate kubuntu-driver-manager libreoffice libreoffice-style-breeze ttf-mscorefonts-installer kubuntu-restricted-extras gufw syncthing"
 neon_dep_pac="python-pip python3-pip build-essential python3-dev libdbus-glib-1-dev libgirepository1.0-dev libcairo2-dev python3-venv python3-wheel qtbase5-dev qtwebengine5-dev extra-cmake-modules qtdeclarative5-dev libkf5windowsystem-dev libkf5plasma-dev libsm-dev libqt5x11extras5-dev"
-mageia_pac="flatpak fwupd"
+mageia_pac="flatpak fwupd nextcloud-client nextcloud-client-dolphin keepassxc kate"
 mageia_dep_pac="python3-cairo-devel python-gobject-devel python3-blockdev gobject-introspection lib64dbus-glib-devel lib64python3-devel make lib64x11-devel python-dbus-devel"
 
 # apt install applications
@@ -41,11 +41,18 @@ then
 		sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 fi 
 
-sudo flatpak install flathub org.nextcloud.Nextcloud -y
-sudo flatpak install flathub org.keepassxc.KeePassXC -y
+if [ $1 == "neon" ]
+then 
+		sudo flatpak install flathub org.nextcloud.Nextcloud -y
+		sudo flatpak install flathub org.keepassxc.KeePassXC -y
+fi
 sudo flatpak install flathub org.qutebrowser.qutebrowser -y
 sudo flatpak install flathub com.valvesoftware.Steam -y
 sudo flatpak install flathub com.visualstudio.code -y
 
 # sudo pip install ntfy
 bash $HOME/repositories/scripts/native_tridactyl.sh
+
+# removing software
+
+sudo $pacmanager remove -y kwrite  
