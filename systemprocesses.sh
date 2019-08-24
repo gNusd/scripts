177903903d0d59d7064fae4cee28c4c935d82402
gnus <gnusd@telia.com>
2019-08-24 11:45:59 +0200

    2019-08-24

--- systemprocesses.sh
+++ systemprocesses.sh
@@ -7,7 +7,7 @@ script_dir=$basedir/scripts
 
 source $script_dir/config
 
-sudo start tlp && echo "$timestamp started tlp" >> $log
+sudo tlp start && echo "$timestamp started tlp" >> $log
 sudo echo "vm.swapiness = 10" >> /etc/sysctl.conf  && echo "$timestamp swapiness = 10" >> $log 
 sudo systemctl enable syncthing@$USER.service  && echo "$timestamp started syncthing@$USER.service" >> $log
 
