c0a13e2b6c3a1cce2dd6c9117162ad0cbdab0cfb
gnus <gnusd@telia.com>
2019-08-24 09:19:10 +0200

    2019-08-24

--- init.sh
+++ init.sh
@@ -65,6 +65,7 @@ git clone git@github.com:gNusd/local-bin.git $HOME/bin && echo "$timestamp clone
 $shell $script_dir/tmux-nvim.sh
 $shell $script_dir/native_tridactyl.sh
 $shell $script_dir/git_repos.sh $dist
+$shell $script_dir/systemprocesses.sh $dist
 
 rm $HOME/init.sh &&  echo "$timestamp removed install script" >> $log
 
--- systemprocesses.sh
+++ systemprocesses.sh
@@ -10,3 +10,15 @@ source $script_dir/config
 sudo start tlp && echo "$timestamp started tlp" >> $log
 sudo echo "vm.swapiness = 10" >> /etc/sysctl.conf  && echo "$timestamp swapiness = 10" >> $log 
 sudo systemctl enable syncthing@$USER.service  && echo "$timestamp started syncthing@$USER.service" >> $log
+
+if [ $1 == "mageia" ]
+then 
+    echo "if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then" >> $HOME/.bashrc
+    echo "exec tmux" >> $HOME/.bashrc
+    echo "fi" && echo "$timestamp added tmux autostart to .bashrc" >> $log
+    echo "tmux source-file $HOME/.tmux.conf" >> $HOME/.bashrc  && echo "$timestamp adding sourcing of .tmux.conf to .bashrc" >> $log
+    echo "source $HOME/.ssh/ssh_alias" >> $HOME/.bashrc && echo "$timestamp added ssh_alias to .bashrc" >> $log
+    echo "if [ -f ~/.bash_aliases ]; then" >> $HOME/.bashrc 
+    echo ". ~/.bash_aliases" >> $HOME/.bashrc 
+    echo "fi" >> $HOME/.bashrc && echo "$timestamp added .bash_alias to .bashrc" >> $log
+fi
